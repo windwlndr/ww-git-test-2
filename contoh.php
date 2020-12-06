a233efef7bb1fdbc86f73cebc674d6cca5f82218
windwlndr <winduwlndr@gmail.com>
2020-12-06 17:54:44 +0700

    menambahkan list

--- contoh.php
+++ contoh.php
@@ -8,5 +8,10 @@
 <body>
     <h1>Hello, ini part 2</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error fugit porro ratione adipisci dolorum dolorem architecto qui quibusdam, molestias exercitationem aspernatur perspiciatis ad, amet facere quis quia corrupti? Nisi, culpa!</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+        <li>tiga</li>
+    </ul>
 </body>
 </html>
